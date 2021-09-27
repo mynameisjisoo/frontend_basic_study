aec8a08e53c1471c5307e05cd50720b5b52989b1
mynameisjisoo <devjisoolee@gmail.com>
2021-09-28 00:36:02 +0900

    Create makeItem function section__item 동적으로 생성하는 function 작성함

--- mini-shoppingmall/main.js
+++ mini-shoppingmall/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const logo = document.querySelector('.logo');
+const itemList = document.querySelector('.section');
 const shirt = document.querySelector('.nav__top');
 const pants = document.querySelector('.nav__pants');
 const skirt = document.querySelector('.nav__skirt');
@@ -8,13 +9,33 @@ const blue = document.querySelector('.nav__blue');
 const yellow = document.querySelector('.nav__yellow');
 const pink = document.querySelector('.nav__pink');
 
-function classificate() {
-  let clothes = {
-    color: ['blue', 'pink', 'yellow'],
-    kinds: ['top', 'pants', 'skirt'],
-    gender: ['man', 'female'],
-    size: ['small', 'large']
-  };
-  //   console.log(clothes.color);
+function makeItem(imgPath, gender, size) {
+  const clothes = document.createElement('img');
+  clothes.setAttribute('src', imgPath);
+  clothes.setAttribute('class', 'clothes');
+
+  const info = document.createElement('span');
+  info.setAttribute('class', 'info');
+  info.innerText = `${gender}, ${size}`;
+
+  const item = document.createElement('article');
+  item.setAttribute('class', 'section__item');
+  item.appendChild(clothes);
+  item.appendChild(info);
+  itemList.appendChild(item);
 }
-classificate();
+makeItem('./imgs/blue_p.png', 'female', 'small');
+
+function getSrc() {
+  const item = {
+    color: ['blue', 'yellow', 'pink'],
+    kinds: ['pants', 'top', 'skirt']
+  };
+  for (let i = 0; i < item.color.length; i++) {
+    for (let j = 0; j < item.kinds.length; j++) {
+      console.log(item['color'][0]);
+    }
+  }
+}
+
+getSrc();
