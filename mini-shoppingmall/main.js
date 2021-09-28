f30d538d7ac837ea5c7d6aa813c822d6dfc40b0c
mynameisjisoo <devjisoolee@gmail.com>
2021-09-28 22:53:33 +0900

    Json 파일에서 동적으로 데이터 받아옴 마크업, 스타일링 변경, json 으로 데이터 받는 메소드 정의함

--- mini-shoppingmall/main.js
+++ mini-shoppingmall/main.js
@@ -1,41 +1,24 @@
 'use strict';
-
-const logo = document.querySelector('.logo');
-const itemList = document.querySelector('.section');
-const shirt = document.querySelector('.nav__top');
-const pants = document.querySelector('.nav__pants');
-const skirt = document.querySelector('.nav__skirt');
-const blue = document.querySelector('.nav__blue');
-const yellow = document.querySelector('.nav__yellow');
-const pink = document.querySelector('.nav__pink');
-
-function makeItem(imgPath, gender, size) {
-  const clothes = document.createElement('img');
-  clothes.setAttribute('src', imgPath);
-  clothes.setAttribute('class', 'clothes');
-
-  const info = document.createElement('span');
-  info.setAttribute('class', 'info');
-  info.innerText = `${gender}, ${size}`;
-
-  const item = document.createElement('article');
-  item.setAttribute('class', 'section__item');
-  item.appendChild(clothes);
-  item.appendChild(info);
-  itemList.appendChild(item);
-}
-makeItem('./imgs/blue_p.png', 'female', 'small');
-
-function getSrc() {
-  const item = {
-    color: ['blue', 'yellow', 'pink'],
-    kinds: ['pants', 'top', 'skirt']
-  };
-  for (let i = 0; i < item.color.length; i++) {
-    for (let j = 0; j < item.kinds.length; j++) {
-      console.log(item['color'][0]);
-    }
-  }
+//Fetch the items from the JSON file
+//loadItem() : fetch를 이용해서 data를 받아오고, 성공적으로 받아오면 json으로 변환하고, json안에있는 items를 리턴함
+function loadItems() {
+  return fetch('data/data.json') //fetch는 data를 성공적으로 받아오면 response라는 객체 전달해줌
+    .then(response => response.json()) //response를 json()이라는 api를 이용해서 response body를 json의 object로 변환함
+    .then(json => json.items); //json에 있는 items만 전달
 }
 
-getSrc();
+//받아온 items를 html 요소로 만들어서 페이지에 표시되게
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item));
+}
+
+//loadItems : json 파일을 동적으로 읽어오는 함수인데 시간이 오래 걸리니까 promise를 리턴함
+loadItems() //
+  .then(items => {
+    console.log(items);
+    //성공적으로 되면 items받아옴
+    displayItems(items); //displayItems: items를 html에 보여줌
+    // setEventListener(items); //btn누르면 filtering
+  })
+  .catch(console.log); //실패할 경우 예외처리
