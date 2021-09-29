1bfa03ed5eed66d4912d60817a7fbc855b22e238
mynameisjisoo <devjisoolee@gmail.com>
2021-09-29 23:09:51 +0900

    Create function onButtonClick 클릭한 버튼에 맞워서 필터링된 아이템만 만들어지도록 함

--- mini-shoppingmall/src/main.js
+++ mini-shoppingmall/src/main.js
@@ -6,6 +6,7 @@ function loadItems() {
   return fetch('data/data.json') //fetch는 data를 성공적으로 받아오면 response라는 객체 전달해줌
     .then(response => response.json()) //response를 json()이라는 api를 이용해서 response body를 json의 object로 변환함
     .then(json => json.items); //json에 있는 items만 전달
+  // .then(items => console.log(items));
 }
 
 //Update the list with the given items
@@ -28,12 +29,30 @@ function createHTMLString(item) {
     `;
 }
 
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+  if (key == null || value == null) {
+    return;
+  }
+  const filterdItem = items.filter(item => item[key] === value);
+  displayItems(filterdItem);
+}
+
+function setEventListener(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 //loadItems : json 파일을 동적으로 읽어오는 함수인데 시간이 오래 걸리니까 promise를 리턴함
 //main
 loadItems()
   .then(items => {
-    //성공적으로 되면 items받아옴
+    //fetch 성공적으로 되면 items받아옴
     displayItems(items); //displayItems: items를 html에 보여줌
-    // setEventListener(items); //btn누르면 filtering
+    setEventListener(items); //btn누르면 filtering
   })
   .catch(console.log); //실패할 경우 예외처리
